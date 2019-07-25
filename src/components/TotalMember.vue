81e841e370c8113d2214823fa6eb54a325d21db7
Ryuhojin <interarea@naver.com>
2019-07-25 17:20:55 +0900

    기능 : 관리자페이지 그래프 노출 및 리스트 노출

---
+++ src/components/TotalMember.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <material-stats-card
+      color="green"
+      icon="mdi-store"
+      title="Revenue"
+      value="$34,245"
+      sub-icon="mdi-calendar"
+      sub-text="Last 24 Hours"
+    />
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>

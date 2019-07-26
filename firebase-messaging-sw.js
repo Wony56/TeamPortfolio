1bdcec1e16b0439376b5f7189396e9f4a75c2342
Hyeon Jin <choo0618@naver.com>
2019-07-26 14:37:08 +0900

    기능: PUSH 알림 구현
    1. PUSH 알림 허용/차단 메세지 등장시 발생하는 에러 수정

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,16 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
  
 // Initialize Firebase
-const config = {
-	apiKey: "AIzaSyD4WyDVLvJV1ZXAH2KOUQDs2JFtQjqv3Do",
-	authDomain: "ssafy-web-pjt.firebaseapp.com",
-	databaseURL: "https://ssafy-web-pjt.firebaseio.com",
-	projectId: "ssafy-web-pjt",
-	storageBucket: "ssafy-web-pjt.appspot.com",
-	messagingSenderId: "363817638878",
-	appId: "1:363817638878:web:aea49aa578e86b59"
-};
-firebase.initializeApp(config);
+ebase.initializeApp(config);
  
 const messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(function(payload){

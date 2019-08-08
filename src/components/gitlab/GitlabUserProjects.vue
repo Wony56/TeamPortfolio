521045ca71c3d6adad9272af751e749ca85f951a
Ryuhojin <interarea@naver.com>
2019-08-08 12:11:41 +0900

    디자인 수정

--- src/components/gitlab/GitlabUserProjects.vue
+++ src/components/gitlab/GitlabUserProjects.vue
@@ -22,18 +22,26 @@
                   {{element.position}},
                   {{element.birth}}
                 </font>
+
+                <div>
+                </div>
               </span>
-              <figure class="highlight__figure" @mouseover="mouseOver(index)">
+              <figure class="highlight__figure">
                 <v-img :src="getImgUrl(element.imgUrl)" aspect-ratio="1" width="200" height="280"></v-img>
               </figure>
+              <div>
+              <v-btn flat style="backgroundcolor:#ff6f61; color:#fff" @click="showSkill(index)">SKILL</v-btn>
+              <v-btn flat style="backgroundcolor:#ff6f61; color:#fff" @click="showGit(index)">GIT</v-btn>
+              </div>
             </div>
           </a>
         </v-flex>
       </v-layout>
     </v-container>
 
+<!-- ============================================================================================================= -->
     <v-layout justify-center>
-      <v-dialog v-model="dialog" max-width="800">
+      <v-dialog v-model="dialog1" max-width="500">
         <v-card v-for="(data, index) in datas" :key="index">
           <v-card-title class="headline">{{data.name_with_namespace}}</v-card-title>
           <v-card-text>{{data.created_at}}</v-card-text>
@@ -41,12 +49,86 @@
           <v-card-actions>
             <v-spacer></v-spacer>
 
-            <v-btn color="blue darken-1" text @click="showRepoPage(data.web_url)">CLOSE</v-btn>
+            <v-btn style="background-color:#ff6f61; color:#fff" flat text @click="showRepoPage(data.web_url)">OPEN</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+<!-- ============================================================================================================= -->
+    <v-layout justify-center>
+      <v-dialog v-model="dialog2" max-width="500">
+        <v-card>
+          <v-card-title style="background-color:#ff6f61; color:#fff" class="headline">SKILL</v-card-title>
+
+          <v-card-text>
+            <article class="skillrow">
+              <ul>
+                <li class="html">
+                  <em>HTML/HTML5</em>
+                  <div>
+                    <span class="animated fadeInLeft"></span>
+                  </div>
+                </li>
+                <li class="css">
+                  <em>CSS/CSS3</em>
+                  <div>
+                    <span class="animated fadeInLeft"></span>
+                  </div>
+                </li>
+                <li class="php">
+                  <em>PHP/OOP</em>
+                  <div>
+                    <span class="animated fadeInLeft"></span>
+                  </div>
+                </li>
+                <li class="mysql">
+                  <em>MYSQL</em>
+                  <div>
+                    <span class="animated fadeInLeft"></span>
+                  </div>
+                </li>
+              </ul>
+            </article>
+            <article class="skillrow">
+              <ul>
+                <li class="jquery">
+                  <em>JQUERY</em>
+                  <div>
+                    <span class="animated fadeInLeft"></span>
+                  </div>
+                </li>
+                <li class="javascript">
+                  <em>JAVASCRIPT</em>
+                  <div>
+                    <span class="animated fadeInLeft"></span>
+                  </div>
+                </li>
+                <li class="seo">
+                  <em>SEO</em>
+                  <div>
+                    <span class="animated fadeInLeft"></span>
+                  </div>
+                </li>
+                <li class="unix">
+                  <em>UNIX</em>
+                  <div>
+                    <span class="animated fadeInLeft"></span>
+                  </div>
+                </li>
+              </ul>
+            </article>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn flat style="background-color:#ff6f61; color:#fff" @click="dialog2 = false">CLOSE</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
     </v-layout>
   </v-container>
+<!-- ============================================================================================================= -->
 </template>
 
 <script>
@@ -58,7 +140,8 @@ export default {
     return {
       datas: [],
       focus: 0,
-      dialog: false
+      dialog1: false,
+      dialog2: false
     };
   },
   methods: {
@@ -75,7 +158,7 @@ export default {
     showRepoPage(url) {
       window.open(url, "_blank");
     },
-    mouseOver(index) {
+    showGit(index) {
       this.getRepositories(
         this.$store.state.memberData[index].id,
         this.$store.state.memberData[index].token,
@@ -84,7 +167,11 @@ export default {
         console.log(data);
         this.datas = data;
       });
-      this.dialog = true;
+      this.dialog1 = true;
+    },
+    showSkill() {
+
+      this.dialog2 = true;
     }
   }
 };
@@ -193,4 +280,80 @@ figure {
 .subtitles:hover {
   color: #ff6f61;
 }
+
 </style>
+<style>
+.skillrow{
+  width: 50%;
+  min-height: 1px;
+  position: relative;
+  display: inline-block;
+}.skillrow ul {
+  margin: 0 20px 0 0 !important;
+  text-align: left !important;
+  padding: 0;
+}.skillrow ul li{
+  list-style: none !important;
+  
+}.skillrow ul li em{
+  display: block;
+  margin-bottom: 15px;
+}.skillrow ul li div{
+  margin-bottom: 35px;
+  border-radius: 3px 3px 3px 3px;
+  -webkit-border-radius: 3px 3px 3px 3px;
+  background: #fafafa;
+  height: 42px;
+  overflow: hidden;
+}.skillrow ul li div span{
+  height: 42px;
+  width: 10%;
+  display: block;
+  border-radius: 3px 3px 3px 3px;
+  -webkit-border-radius: 3px 3px 3px 3px;
+}
+.skillrow ul li.html div span { background: #E13C4C; width: 90%; }
+.skillrow ul li.css div span { background: #ffdb43; width: 90%; }
+.skillrow ul li.php div span { background: #9D2027; width: 80%; }
+.skillrow ul li.mysql div span { background: #3498db; width: 75%; }
+.skillrow ul li.jquery div span { background: #2ecc71; width: 60%; }
+.skillrow ul li.javascript div span { background: #A94034 }
+.skillrow ul li.seo div span { background: #FF8137; width: 60%; }
+.skillrow ul li.unix div span { width: 65%; background: #0CBFB3; }
+
+.animated {
+  -webkit-animation-duration: 1s;
+  animation-duration: 1s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+}.fadeInLeft {
+  -webkit-animation-name: fadeInLeft;
+  animation-name: fadeInLeft;
+}
+@-webkit-keyframes fadeInLeft {
+  0% {
+    opacity: 0;
+    -webkit-transform: translate3d(-100%, 0, 0);
+    transform: translate3d(-100%, 0, 0);
+  }100% {
+    opacity: 1;
+    -webkit-transform: none;
+    transform: none;
+  }
+}
+@keyframes fadeInLeft {
+  0% {
+    opacity: 0;
+    -webkit-transform: translate3d(-100%, 0, 0);
+    -ms-transform: translate3d(-100%, 0, 0);
+    transform: translate3d(-100%, 0, 0);
+  }100% {
+    opacity: 1;
+    -webkit-transform: none;
+    -ms-transform: none;
+    transform: none;
+  }
+}
+
+</style>
+
